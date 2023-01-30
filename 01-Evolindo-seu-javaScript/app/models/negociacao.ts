86a94a53f6b1a7305469628e5e3c8277c578368f
Aritana Noara Costa Santos <aritanacs@ciandt.com>
2023-01-30 03:06:52 -0300

    declaration in the constructor

--- 01-Evolindo-seu-javaScript/app/models/negociacao.ts
+++ 01-Evolindo-seu-javaScript/app/models/negociacao.ts
@@ -1,13 +1,5 @@
 export class Negociacao {
-  private _data: Date; //private field are accesible on the class constructor from inside the class declaration itself.
-  private _quantidade: number;
-  private _valor: number;
-
-  constructor(data: Date, quantidade: number, valor: number) {
-    this._data = data;
-    this._quantidade = quantidade;
-    this._valor = valor;
-  }
+  constructor(_data: Date, _quantidade: number, _valor: number) {}
 
   get data(): Date {
     return this._data;
