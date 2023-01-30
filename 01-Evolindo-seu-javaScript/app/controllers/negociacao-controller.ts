e663e89d764d7da5054822db9fe9d8074c6afaf6
Aritana Noara Costa Santos <aritanacs@ciandt.com>
2023-01-29 23:16:48 -0300

    clearing the form

--- 01-Evolindo-seu-javaScript/app/controllers/negociacao-controller.ts
+++ 01-Evolindo-seu-javaScript/app/controllers/negociacao-controller.ts
@@ -11,17 +11,25 @@ export class NegociacaoController {
     this.inputValor = document.querySelector("#valor");
   }
 
-  adiciona() {
+  adiciona():void {
     const negociacao = this.criaNegociacao();
     console.log(negociacao);
+    this.limparFormulario();
   }
 
-  criaNegociacao() {
+  criaNegociacao():Negociacao {
     const exp: RegExp = /-/g;
     const date: Date = new Date(this.inputData.value.replace(exp, ","));
     const quantidade: number = parseInt(this.inputQuantidade.value);
     const valor: number = parseFloat(this.inputValor.value);
     const negociacao = new Negociacao(date, quantidade, valor);
     return negociacao;
+  }
+
+  limparFormulario():void{
+    this.inputData.value = '';
+    this.inputQuantidade.value ='';
+    this.inputValor.value = '';
+    this.inputData.focus();
   }
 }
